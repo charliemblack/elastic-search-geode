b21dfcce5579c990489b5c25bbbc9f3bc1eff96c
Charlie Black <cblack@pivotal.io>
2016-09-30 10:14:49 -0700

    Added in a command that run to clear out es

---
+++ logstash/reset_es.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Taken from gradle script.
+# Attempt to set APP_HOME
+# Resolve links: $0 may be a link
+PRG="$0"
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`/" >/dev/null
+APP_HOME="`pwd -P`"
+cd "$SAVED" >/dev/null
+
+curl -XDELETE 'http://localhost:9200/logstash-*'
+
